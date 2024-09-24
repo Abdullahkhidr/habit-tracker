3c3b4cf9f835639411c7b29063e03390d6dfb6ba
Abdullah khidr <Abdullahkhidrmain@gmail.com>
2024-09-24 20:54:49 +0300

    resolve conflicts

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:habit_tracker/core/helpers/app_bloc_observer.dart';
 import 'package:habit_tracker/core/helpers/hive_helper.dart';
@@ -25,25 +26,36 @@ class HabitTracker extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      navigatorKey: _navigator,
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.light().copyWith(
-          primaryColor: kPrimaryColor,
-          scaffoldBackgroundColor: kBackgroundColor,
-          appBarTheme: const AppBarTheme(titleTextStyle: TextStyles.h1),
-          textTheme: GoogleFonts.cairoTextTheme(),
-          colorScheme: const ColorScheme.light().copyWith(
-              primary: kPrimaryColor,
-              secondary: kSecondaryColor,
-              onPrimary: kOnPrimaryColor,
-              onSecondary: kOnSecondaryColor,
-              error: kErrorColor)),
-      home: BlocProvider(
-        create: (context) => HabitEditorBloc(CreateHabitUseCase(
-            repository: HabitEditorRepositoryImpl(
-                habitEditorLocalDataSource: HabitEditorLocalDataSource()))),
-        child: const HabitEditorView(),
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      builder: (context, child) => MaterialApp(
+        navigatorKey: _navigator,
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.light().copyWith(
+            bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+              type: BottomNavigationBarType.fixed,
+              selectedItemColor: kPrimaryColor,
+              unselectedItemColor: Colors.grey,
+              elevation: 20.0,
+              backgroundColor: kOnPrimaryColor,
+            ),
+            primaryColor: kPrimaryColor,
+            scaffoldBackgroundColor: kBackgroundColor,
+            appBarTheme: const AppBarTheme(titleTextStyle: TextStyles.h1),
+            textTheme: GoogleFonts.cairoTextTheme(),
+            colorScheme: const ColorScheme.light().copyWith(
+                primary: kPrimaryColor,
+                secondary: kSecondaryColor,
+                onPrimary: kOnPrimaryColor,
+                onSecondary: kOnSecondaryColor,
+                error: kErrorColor)),
+        home: BlocProvider(
+          create: (context) => HabitEditorBloc(CreateHabitUseCase(
+              repository: HabitEditorRepositoryImpl(
+                  habitEditorLocalDataSource: HabitEditorLocalDataSource()))),
+          child: const HabitEditorView(),
+        ),
       ),
     );
   }
